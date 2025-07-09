4a9a04e2f8279ee343a437b25841eec6f8e6d5c2
sunwon <jsw5913@naver.com>
2025-07-09 12:38:57 +0900

    [Feat] 진행중인 챌린지 다중 선택 및 중단 기능 구현

---
+++ lib/modules/reading_challenge/view_model/ongoing_challenge_view_model.dart
@@ -0,0 +1,75 @@
+import 'package:book/modules/reading_challenge/model/challenge_response.dart';
+import 'package:book/modules/reading_challenge/repository/reading_challenge_repository.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'ongoing_challenge_view_model.freezed.dart';
+part 'ongoing_challenge_view_model.g.dart';
+
+@freezed
+abstract class OngoingChallengeScreenState with _$OngoingChallengeScreenState {
+  const factory OngoingChallengeScreenState({
+    @Default(AsyncData([])) AsyncValue<List<ChallengeResponse>> challenges,
+    @Default(false) bool isSelectionMode,
+    @Default({}) Set<int> selectedChallengeIds,
+  }) = _OngoingChallengeScreenState;
+}
+
+@riverpod
+class OngoingChallengeViewModel extends _$OngoingChallengeViewModel {
+  @override
+  OngoingChallengeScreenState build() {
+    // `build`가 완료된 직후에 `_fetchChallenges`를 실행하도록 변경
+    Future.microtask(_fetchChallenges);
+    return const OngoingChallengeScreenState();
+  }
+
+  Future<void> _fetchChallenges() async {
+    state = state.copyWith(challenges: const AsyncLoading());
+    final repo = ref.read(readingChallengeRepositoryProvider);
+
+    final challenges = await AsyncValue.guard(() async {
+      final res = await repo.getOngoingChallenges();
+      return res.data;
+    });
+
+    state = state.copyWith(challenges: challenges);
+  }
+
+  void toggleSelectionMode() {
+    final newMode = !state.isSelectionMode;
+    state = state.copyWith(
+      isSelectionMode: newMode,
+      // 선택 모드 해제 시 선택 목록 초기화
+      selectedChallengeIds: newMode ? state.selectedChallengeIds : {},
+    );
+  }
+
+  void toggleChallengeSelection(int challengeId) {
+    final newSelection = Set<int>.from(state.selectedChallengeIds);
+    if (newSelection.contains(challengeId)) {
+      newSelection.remove(challengeId);
+    } else {
+      newSelection.add(challengeId);
+    }
+    state = state.copyWith(selectedChallengeIds: newSelection);
+  }
+
+  Future<void> deleteSelectedChallenges() async {
+    final repo = ref.read(readingChallengeRepositoryProvider);
+    final idsToDelete = List<int>.from(state.selectedChallengeIds);
+
+    // TODO: 로딩 상태 처리
+    await Future.wait(
+      idsToDelete.map(
+        (id) => repo.deleteChallenge(id),
+      ),
+    );
+
+    state = state.copyWith(
+      selectedChallengeIds: {},
+      isSelectionMode: false,
+    );
+    ref.invalidateSelf();
+  }
+}
---
+++ lib/modules/reading_challenge/view_model/ongoing_challenge_view_model.g.dart
@@ -0,0 +1,28 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'ongoing_challenge_view_model.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$ongoingChallengeViewModelHash() =>
+    r'3df904fa2a7cb0cd3bd2d9305796ee610271cac3';
+
+/// See also [OngoingChallengeViewModel].
+@ProviderFor(OngoingChallengeViewModel)
+final ongoingChallengeViewModelProvider = AutoDisposeNotifierProvider<
+    OngoingChallengeViewModel, OngoingChallengeScreenState>.internal(
+  OngoingChallengeViewModel.new,
+  name: r'ongoingChallengeViewModelProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$ongoingChallengeViewModelHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$OngoingChallengeViewModel
+    = AutoDisposeNotifier<OngoingChallengeScreenState>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
