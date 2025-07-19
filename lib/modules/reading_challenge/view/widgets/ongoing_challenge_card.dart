e693c662e5f972aaa8fed70abe2ddecffc565188
doyou1 <tla149@daum.net>
2025-07-19 20:44:38 +0900

    [Feat] #57 리딩챌린지:완독 도서 화면, 도서 클릭시 도서 상세로 이동 - `GestureDetector` 중복 정의 제거

--- lib/modules/reading_challenge/view/widgets/ongoing_challenge_card.dart
+++ lib/modules/reading_challenge/view/widgets/ongoing_challenge_card.dart
@@ -32,66 +32,61 @@ class OngoingChallengeCard extends StatelessWidget {
             },
           );
           context.push(uri.toString());
+        } else {
+          onToggle();
         }
       },
-      child:GestureDetector(
-        onTap: () {
-          if (isSelectionMode) {
-            onToggle();
-          }
-        },
-        child: ClipRRect(
-          borderRadius: BorderRadius.circular(8),
-          child: AspectRatio(
-            aspectRatio: 2 / 3,
-            child: Stack(
-              fit: StackFit.expand,
-              children: [
-                CachedNetworkImage(
-                  imageUrl: challenge.book.thumbnailUrl,
-                  fit: BoxFit.cover,
-                  errorWidget: (context, url, error) => const Icon(Icons.error),
-                ),
-                // Gradient for text readability
-                Positioned.fill(
-                  child: DecoratedBox(
-                    decoration: BoxDecoration(
-                      gradient: LinearGradient(
-                        begin: Alignment.topCenter,
-                        end: Alignment.bottomCenter,
-                        colors: [
-                          Colors.transparent,
-                          Colors.black.withOpacity(0.1),
-                          Colors.black.withOpacity(0.8),
-                        ],
-                        stops: const [0.6, 0.8, 1.0],
-                      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(8),
+        child: AspectRatio(
+          aspectRatio: 2 / 3,
+          child: Stack(
+            fit: StackFit.expand,
+            children: [
+              CachedNetworkImage(
+                imageUrl: challenge.book.thumbnailUrl,
+                fit: BoxFit.cover,
+                errorWidget: (context, url, error) => const Icon(Icons.error),
+              ),
+              // Gradient for text readability
+              Positioned.fill(
+                child: DecoratedBox(
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Colors.transparent,
+                        Colors.black.withOpacity(0.1),
+                        Colors.black.withOpacity(0.8),
+                      ],
+                      stops: const [0.6, 0.8, 1.0],
                     ),
                   ),
                 ),
-                // Title text
-                Positioned(
-                  bottom: 8,
-                  left: 8,
-                  right: 8,
-                  child: Text(
-                    challenge.book.title,
-                    style: AppTexts.b7.copyWith(color: Colors.white),
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                  ),
+              ),
+              // Title text
+              Positioned(
+                bottom: 8,
+                left: 8,
+                right: 8,
+                child: Text(
+                  challenge.book.title,
+                  style: AppTexts.b7.copyWith(color: Colors.white),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
                 ),
-                if (isSelectionMode)
-                  Positioned(
-                      top: 11,
-                      right: 8,
-                      child: CheckBox1(
-                        value: isSelected,
-                        onChanged: (bool value) => onToggle(),
-                      )
-                  )
-              ],
-            ),
+              ),
+              if (isSelectionMode)
+                Positioned(
+                    top: 11,
+                    right: 8,
+                    child: CheckBox1(
+                      value: isSelected,
+                      onChanged: (bool value) => onToggle(),
+                    )
+                )
+            ],
           ),
         ),
       ),
