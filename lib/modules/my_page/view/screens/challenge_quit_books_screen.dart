b33f87d1f6904e29c881b51f761a27e4b9f11ac8
June Shim <june.shim2@gmail.com>
2025-07-29 18:17:31 +0900

    [-] 챌린지 중단 도서 버튼 수정

--- lib/modules/my_page/view/screens/challenge_quit_books_screen.dart
+++ lib/modules/my_page/view/screens/challenge_quit_books_screen.dart
@@ -1,6 +1,7 @@
 import 'package:book/common/components/form/book_with_checkbok.dart';
 import 'package:book/common/components/button/cta_button_l1.dart';
 import 'package:book/common/theme/style/app_paddings.dart';
+import 'package:book/gen/colors.gen.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:book/modules/book_log/model/book_log_models.dart';
@@ -48,39 +49,67 @@ class _ChallengeQuitBooksScreenState extends State<ChallengeQuitBooksScreen> {
           ),
         ],
       ),
-      body: Padding(
-        padding: AppPaddings.SCREEN_BODY_PADDING.copyWith(left: 0, right: 0),
-        child: ListView.builder(
-          itemCount: books.length,
-          itemBuilder: (context, index) {
-            final book = books[index];
-            return BookWithCheckbox(
-              thumbnailUrl: book.imageUrl,
-              title: book.title,
-              author: book.author,
-              lastReadAt: book.lastReadAt,
-              checked: checkedList[index],
-              onChanged: (v) {
-                setState(() {
-                  checkedList[index] = v;
-                });
+      body: Stack(
+        children: [
+          Padding(
+            padding:
+                AppPaddings.SCREEN_BODY_PADDING.copyWith(left: 0, right: 0),
+            child: ListView.builder(
+              itemCount: books.length,
+              itemBuilder: (context, index) {
+                final book = books[index];
+                return BookWithCheckbox(
+                  thumbnailUrl: book.imageUrl,
+                  title: book.title,
+                  author: book.author,
+                  lastReadAt: book.lastReadAt,
+                  checked: checkedList[index],
+                  onChanged: (v) {
+                    setState(() {
+                      checkedList[index] = v;
+                    });
+                  },
+                );
               },
-            );
-          },
-        ),
-      ),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.fromLTRB(14, 0, 14, 54),
-        child: CtaButtonL1(
-          text: '삭제하기',
-          enabled: checkedList.contains(true),
-          onPressed: checkedList.contains(true)
-              ? () {
-                  // TODO: 삭제 처리 로직 추가
-                  print('삭제하기 버튼 클릭됨');
-                }
-              : null,
-        ),
+            ),
+          ),
+          Positioned(
+            left: 0,
+            right: 0,
+            bottom: 0,
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(14, 0, 14, 54),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  CtaButtonL1(
+                    text: '다시 시작하기',
+                    enabled: checkedList.contains(true),
+                    onPressed: checkedList.contains(true)
+                        ? () {
+                            // TODO: 다시 시작하기 처리 로직 추가
+                            print('다시 시작하기 버튼 클릭됨');
+                          }
+                        : null,
+                  ),
+                  const SizedBox(height: 8),
+                  CtaButtonL1(
+                    text: '삭제하기',
+                    enabled: checkedList.contains(true),
+                    onPressed: checkedList.contains(true)
+                        ? () {
+                            // TODO: 삭제 처리 로직 추가
+                            print('삭제하기 버튼 클릭됨');
+                          }
+                        : null,
+                    backgroundColor: ColorName.g7,
+                    borderColor: ColorName.g6,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
